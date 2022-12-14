c7a85389e15cb9b8f4f508f817bcc19d374ad6c1
JuliusJak <113606334+JuliusJak@users.noreply.github.com>
2022-12-14 10:54:06 +0100

    Fixed 'Char' in GameLogic (#9)

--- hangman_client/src/main/java/yh/fabulousstars/hangman/GameLogic.java
+++ hangman_client/src/main/java/yh/fabulousstars/hangman/GameLogic.java
@@ -1,28 +1,32 @@
 package yh.fabulousstars.hangman;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class GameLogic  {
-    public Char newLetter;
+    public String newLetter;
     public int guessedLetterPosition;
-    public List<Char> gussedLetters; // = new ArrayList<>;
-    public List<Char> missedLetters; // = new ArrayList<>;
+    public List<Character> gussedLetters; // = new ArrayList<>;
+    public List<Character> missedLetters; // = new ArrayList<>;
 
     /**
      * SetPlayerTurn is a method to give the the play turn for specific player according to games rules.
      * @param playerIndex is the number of the player in the player list
      */
+    /*
     public SetPlayerTurn (int playerIndex){  // NOT FINISH ..... i think this method can be delete! there is no use for it.
 
         return true;
     }
 
+     */
+
     /**
      * GetGuess is a method to get a letter from player as a new guess for new letter
      * @return this method return the letter that the player entered
      */
-    public Char GetGuess (){  //This method will ask for nee letter from the palyer and returns this letter.
+    public String GetGuess (){  //This method will ask for nee letter from the palyer and returns this letter.
         Scanner scanner = new Scanner(System.in);
         this.newLetter = scanner.nextLine(); // assign the letter from the user to the paramaeter.
 
@@ -36,7 +40,7 @@ public class GameLogic  {
      * @param currentWord the hidden word
      * @return true and assigns the the letter position to the paramater or return false in case its wrong guess
      */
-    public boolean CompareGuess (Char guessLetter, List<Char> currentWord){ //This method compares between the new letter and check if its exist in the hidden word
+    public boolean CompareGuess (Character guessLetter, List<Character> currentWord){ //This method compares between the new letter and check if its exist in the hidden word
         boolean res = currentWord.contains(guessLetter);
         if (res == true) {
             this.guessedLetterPosition = currentWord.indexOf(guessLetter)+1; // this line gets the letter position in the word, +1 because list index starts with 0
@@ -62,30 +66,27 @@ public class GameLogic  {
      * @param missedGuessList this is the list of total wrong guess
      * @return true in case the worng guess exceded the maximum allowed, returns false if still more guesses available.
      */
-    public boolean CheckHanged (List<Char> missedGuessList){
-        if missedGuessList.size >= 10 { // missedGuessList is the player list of missed guess. The number (10) is according to the steps of hangman and can be changed accordingly
-            return true // return true if the player reached the maximum allowed wrong guess
-        }else{
-            return false
-        }
 
         /**
          * this methiod prints out the game instruction
          */
         public static void GameInstruction(){
-            System.out.println('*******************************************');
-            System.out.println('WELCOME TO FABULOUS STARS HANGMAN GAME');
-            System.out.println('This game is between multiplayers');
-            System.out.println('Each player will take his turn to choose \n a word that need to be guessed by other players');
-            System.out.println('Once you get hanged you will loose');
-            System.out.println('The winner is the LAST MAN STANDING');
-            System.out.println(*******************************************);
+            System.out.println("*******************************************");
+            System.out.println("WELCOME TO FABULOUS STARS HANGMAN GAME");
+            System.out.println("This game is between multiplayers");
+            System.out.println("Each player will take his turn to choose \n a word that need to be guessed by other players");
+            System.out.println("Once you get hanged you will loose");
+            System.out.println("The winner is the LAST MAN STANDING");
+            System.out.println("*******************************************");
         }
 
         /**
          * this method to announce the winner of the game
          */
+        /*
         public String resutl () { // need more information from the class PLAYERS to see the players list structure and decide the result
 
         }
+
+         */
 }
